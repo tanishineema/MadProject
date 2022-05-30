c2f33e7005de5a5cdbd7ea747fb7a9cbba1d85e2
Tanishi Neema <92111930+tanishineema@users.noreply.github.com>
2022-05-30 11:55:57 +0530

    project

---
+++ app/src/main/java/com/example/madproject/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.madproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return true;
+    }
+}
